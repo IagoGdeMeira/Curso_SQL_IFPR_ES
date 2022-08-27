389ff037c7d13e172f7d756edbbbba9e3bc29eec
Iago Gonçalves de Meira <iagomeiraifpr@gmail.com>
2022-08-27 15:26:23 -0300

    Primeira lição do curso

---
+++ Script - 01 #02.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE teste_bd;
+CREATE DATABASE IF NOT EXISTS teste_bd;
+
+DROP DATABASE teste_bd;
+DROP DATABASE IF EXISTS teste_bd;
+
+CREATE DATABASE teste_bd_2;
+
+USE teste_bd;
+
+CREATE TABLE estado (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL UNIQUE,
+    sigla CHAR(2) NOT NULL UNIQUE,
+    ativo CHAR(1) NOT NULL DEFAULT 'S',
+    data_cadastro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT checar_ativo CHECK(ativo IN('S', 'N'))
+);
+
+INSERT INTO estado(nome, sigla) VALUES('BAHIA','BA');
+INSERT INTO estado(nome, sigla) VALUES('PARANÁ','PR');
+INSERT INTO estado(nome, sigla) VALUES('SÃO PAULO','SP');
+INSERT INTO estado(nome, sigla) VALUES('RIO DE JANEIRO','RJ');
+INSERT INTO estado(nome, sigla) VALUES('RIO GRANDE DO SUL','RS');
+
+SELECT id, nome, sigla, ativo, data_cadastro FROM estado;
